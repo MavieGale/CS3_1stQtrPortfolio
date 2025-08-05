ee83ca675431e1b85cebad527e40af8e4600bf88
MavieGale <b2029mggcruz@pshs.edu.ph>
2025-08-05 08:45:19 +0800

    Final Commit

---
+++ public/script.js
@@ -0,0 +1,10 @@
+// no script tag needed here 
+// use let for variables :))
+
+let age = 14;
+let maxAge = 100;
+let numPerDay = 50;
+
+let totalRequired = (numPerDay * 365) * (maxAge - age)
+
+document.write ("You will need " + totalRequired + " to last you until the ripe old age of X.")
