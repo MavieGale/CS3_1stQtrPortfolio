e05abc7045935aa5d6863288b190ae279e26bfa5
MavieGale <b2029mggcruz@pshs.edu.ph>
2025-08-12 08:55:48 +0800

    commit

--- public/script.js
+++ public/script.js
@@ -12,3 +12,15 @@ if (confirm(`Is ${maxAge}, ${numPerDay}, ${age} really your numbers?`)){
 else {
     console.log("Run the code again!")
 }
+
+let maxAge = window.prompt ("Enter your max age: ", "100")
+let numPerDay = window.prompt ("Enter your number per day", "100") 
+let age = window.prompt ("Enter your age:", "14")
+
+if (confirm(`Is ${maxAge}, ${numPerDay}, ${age} really your numbers?`)){
+    console.log("okay");
+}
+
+else {
+    console.log("Run the code again!")
+} */
