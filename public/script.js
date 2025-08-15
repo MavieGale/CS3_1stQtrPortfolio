b5e7fddd8509e2d17e713452b6d5d907a388fec6
MavieGale <b2029mggcruz@pshs.edu.ph>
2025-08-15 12:11:12 +0800

    Wrong commit

--- public/script.js
+++ public/script.js
@@ -1,15 +1,29 @@
-function getData() {
-    let perDay = document.getElementById("numPerDay").value;
-    let age = window.prompt("Enter your current age:", "14");
-    let maxAge = window.prompt("Enter your max age:", "100");
-  
-    let sure = window.confirm(`Are you sure about these numbers?\nAge: ${age}\nMax Age: ${maxAge}\nSnacks per day: ${perDay}`);
-  
-    if (sure) {
-      let totalRequired = Number(perDay) * 365 * (Number(maxAge) - Number(age));
-      document.getElementById("result").innerHTML = `Result: You will need ${totalRequired} snacks to last you until the ripe old age of ${maxAge}.`;
-    } else {
-      window.alert("Please click the button again and re-enter your values.");
-    }
-  }
+<body> 
+    <h1>Feed Me My Snacks</h1>
+
+    <label for="age">Enter your age
+        <input id="age"/>
+    </label>
+</br/>
+
+    <label for="maxAge">Enter the max age you would like
+        <input id="maxAge" />
+    </label>
+</br/>
+    <label for="numPerDay">Enter how much of your favorite snack you consume in a day
+        <input id="numPerDay" />
+    </label>
+</br/>
+
+    <><p id="result"> Result :</p><button onclick="getData()"> Accept invitation</button><script>
+        function getData(){let} Age = age.value;
+        let maxAGE = document.getElementById("maxAge").value;
+        let perDay = document.getElementById("numPerDay").value;
+        if(confirm(`Are ${Age}, ${maxAge}, and ${numPerDay} is this really your inputs?`)){let} totalRequired= (Number(perDay)*365) * (Number(maxAGE) - Number (AGE));
+        result.innerHTML = "Result: You will need " +totalRequired + "to last you until the ripe old age of " + maxAGE;
+        {"}"}
+        else{window.alert("Please click to run again")}
+    </script></>
+</body>s
+
   
