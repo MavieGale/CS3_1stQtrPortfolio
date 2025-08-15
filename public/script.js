f162857684205efd4c99004275dc58a0a5af02f8
MavieGale <b2029mggcruz@pshs.edu.ph>
2025-08-15 12:12:34 +0800

    Commit huhu

--- public/script.js
+++ public/script.js
@@ -4,16 +4,14 @@
     <label for="age">Enter your age
         <input id="age"/>
     </label>
-</br/>
 
     <label for="maxAge">Enter the max age you would like
         <input id="maxAge" />
     </label>
-</br/>
+    
     <label for="numPerDay">Enter how much of your favorite snack you consume in a day
         <input id="numPerDay" />
     </label>
-</br/>
 
     <><p id="result"> Result :</p><button onclick="getData()"> Accept invitation</button><script>
         function getData(){let} Age = age.value;
